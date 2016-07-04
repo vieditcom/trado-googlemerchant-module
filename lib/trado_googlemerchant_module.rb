335520c7b02b7ef89400c6ec77d3e393b9d0bc85
Tom Dallimore <me@tomdallimore.com>
2016-07-04 18:24:47 +0700

    Fixed typo,

--- lib/trado_googlemerchant_module.rb
+++ lib/trado_googlemerchant_module.rb
@@ -3,6 +3,6 @@ end
 
 require 'trado_googlemerchant_module/engine'
 require 'trado_googlemerchant_module/version'
-require 'trado_googlemerchant_module/activerecord'
+require 'trado_googlemerchant_module/active_record'
 
 ActiveRecord::Base.send(:include, TradoGooglemerchantModule::ActiveRecord)
